0ce4ec409a95b13571786fa2bce9bfd0ed6d69ac
Anuar <anuarbekkamzinov@gmail.com>
2025-01-20 23:54:59 +0500

    button 'click'

--- script/js.js
+++ script/js.js
@@ -1,1 +1,8 @@
-console.log('Hello')
+const model_btn = document.querySelector('[data-button]');
+console.dir(model_btn);
+
+const modal = document.querySelector('.modal__overlay');
+
+model_btn.addEventListener('click', (e) => {
+    modal.style.display = 'flex';
+})
